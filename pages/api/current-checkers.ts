2fb6b65c7fd65bf92ac0a71df90290ca203b139e
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-02-22 16:10:07 +0100

    hook up res with interface

--- pages/api/current-checkers.ts
+++ pages/api/current-checkers.ts
@@ -1,12 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { scrapeForCurrentCheckers, SCRAPING_URL } from '../../lib/scraping/scraper';
 
-export default async (_req: NextApiRequest, res: NextApiResponse) => {
+interface CurrentCheckersResponse {
+  current: number;
+}
+
+export default async (_req: NextApiRequest, res: NextApiResponse<CurrentCheckersResponse>) => {
   try {
     const data = await scrapeForCurrentCheckers();
-    res.status(200).json(data);
+    res.status(200).json({ current: data });
   } catch (error) {
     console.log(`Error when scraping ${SCRAPING_URL}`, error);
-    res.status(500).json({ error });
+    res.status(500).json({ current: 0 });
   }
 };
