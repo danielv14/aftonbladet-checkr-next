b98e81967aecf56a23ace5b353d96bc68e08eea2
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-02-22 13:54:55 +0100

    include id in the docdata function

--- firebase/utils.ts
+++ firebase/utils.ts
@@ -2,4 +2,6 @@ import * as firebase from 'firebase';
 
 type documentData = firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>;
 
-export const toFireStoreDocData = (doc: documentData) => doc.data();
+export const toFireStoreDocData = (doc: documentData) => {
+  return { id: doc.id, ...doc.data() };
+};
