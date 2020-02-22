cf0ea03a8c365d00687bd25dc371447d4ef8d298
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-02-22 13:55:20 +0100

    add functions file with is to be used to interact with the firestore db

---
+++ firebase/functions.ts
@@ -0,0 +1,13 @@
+import { checkersCollection } from './db';
+import { toFireStoreDocData } from './utils';
+import { Checker } from '../interfaces/Checker';
+
+export const getAllEntries = async () => {
+  try {
+    const querySnapshot = await checkersCollection.get();
+    const checkers = querySnapshot.docs.map(toFireStoreDocData);
+    return checkers as Checker[];
+  } catch (err) {
+    console.log('Error getting documents', err);
+  }
+};
