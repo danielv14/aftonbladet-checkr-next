eb5a682da1226bd37efceeb824aecc1a57758d70
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-02-24 18:19:02 +0100

    fetch testdata when in development

--- pages/api/get-checkers.ts
+++ pages/api/get-checkers.ts
@@ -1,12 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllEntries } from '../../lib/firebase/actions';
-import { Checker } from '../../interfaces/Checker';
+import { CheckerDTO } from '../../interfaces/Checker';
+import { testDataAsCheckerDTO } from '../../utils/testData/checkersTestData';
+import { Environment } from '../../interfaces/Environment';
 
 interface GetCheckersResponse {
-  checkers: Checker[];
+  checkers: CheckerDTO[];
 }
 
 export default async (_req: NextApiRequest, res: NextApiResponse<GetCheckersResponse>) => {
-  const data = await getAllEntries();
+  const data = process.env.NODE_ENV === Environment.Development ? await testDataAsCheckerDTO() : await getAllEntries();
   res.status(200).json({ checkers: data });
 };
