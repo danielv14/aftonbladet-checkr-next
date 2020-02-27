d8e0fb0cd6b404c409ff5578fbe1c51daf1d14e0
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-02-27 18:49:32 +0100

    remove param validation

--- pages/api/seed/add-today.ts
+++ pages/api/seed/add-today.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { validFirebaseApiKey } from '../../../utils/validApiKey';
 import { scrapeForCurrentCheckers } from '../../../lib/scraping/scraper';
 import { getCurrentDate } from '../../../utils/getCurrentDate';
 import { addEntryToDB, getLatestEntry } from '../../../lib/firebase/actions';
@@ -7,26 +6,22 @@ import { FirebaseChecker } from '../../../interfaces/Checker';
 import { isToday } from '../../../utils/isToday';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (validFirebaseApiKey(req.query.apiKey)) {
-    try {
-      const currentEntryForToday = await getLatestEntry();
-      if (currentEntryForToday && isToday(new Date(currentEntryForToday.created))) {
-        res.status(200).json({ status: 200, message: `Todays checkers has already been added into db` });
-        return;
-      }
-
-      const currentCheckers = await scrapeForCurrentCheckers();
-      const checkerObj = {
-        amount: currentCheckers,
-        created: getCurrentDate(),
-      } as FirebaseChecker;
-      await addEntryToDB(checkerObj);
-      res.status(200).json({ status: 200, message: `Today's checkers has been added into db` });
-    } catch (error) {
-      console.log(`Error when adding today's amount of checkers`, error);
-      res.status(500).json({ status: 500, message: error });
+  try {
+    const currentEntryForToday = await getLatestEntry();
+    if (currentEntryForToday && isToday(new Date(currentEntryForToday.created))) {
+      res.status(200).json({ status: 200, message: `Todays checkers has already been added into db` });
+      return;
     }
-  } else {
-    res.status(404).json({ status: 401 });
+
+    const currentCheckers = await scrapeForCurrentCheckers();
+    const checkerObj = {
+      amount: currentCheckers,
+      created: getCurrentDate(),
+    } as FirebaseChecker;
+    await addEntryToDB(checkerObj);
+    res.status(200).json({ status: 200, message: `Today's checkers has been added into db` });
+  } catch (error) {
+    console.log(`Error when adding today's amount of checkers`, error);
+    res.status(500).json({ status: 500, message: error });
   }
 };
