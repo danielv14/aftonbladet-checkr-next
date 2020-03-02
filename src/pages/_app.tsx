e3b43cfefbc8227d6f7fc40ac0135c36a65683f4
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-03-02 18:10:59 +0100

    add head with title and meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import { AppProps } from 'next/app';
 import '../assets/global.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Aftonbladet-Checker</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
