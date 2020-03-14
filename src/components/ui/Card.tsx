f793e4dbce933420d3ee8d046ce2de805e6d76eb
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-03-14 18:46:57 +0100

    implement better typings for theme in styled components Somewhat clunky syntax when working with the theme

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { StyledTheme } from '../../settings/theming/theme';
 
 export interface CardProps {
   height?: number;
   header?: string;
 }
+
+export const Card: React.FC<CardProps> = ({ height = 400, header, children }) => {
+  const wrapperheight = `${height}px`;
+  return (
+    <CardContainer>
+      {header && <CardHeader>{header}</CardHeader>}
+      <CardContentWrapper height={wrapperheight}>{children}</CardContentWrapper>
+    </CardContainer>
+  );
+};
 
 const CardHeader = styled.h2`
   text-align: center;
@@ -16,21 +27,13 @@ const CardContentWrapper = styled.div`
   height: ${props => props.height};
 `;
 
-const CardContainer = styled.div`
-  background: ${({ theme }) => theme.card.background};
-  border-radius: ${({ theme }) => theme.card.borderRadius};
-  padding: ${({ theme }) => theme.card.padding};
+const CardContainer = styled.div(
+  ({ theme }: StyledTheme) => `
+  background: ${theme.card.background};
+  border-radius: ${theme.card.borderRadius};
+  padding: ${theme.card.padding};
   position: relative;
   width: 100%;
-  box-shadow: ${({ theme }) => theme.card.boxShadow};
-`;
-
-export const Card: React.FC<CardProps> = ({ height = 400, header, children }) => {
-  const wrapperheight = `${height}px`;
-  return (
-    <CardContainer>
-      {header && <CardHeader>{header}</CardHeader>}
-      <CardContentWrapper height={wrapperheight}>{children}</CardContentWrapper>
-    </CardContainer>
-  );
-};
+  box-shadow: ${theme.card.boxShadow};
+  `,
+);
---
+++ styles.d.ts
@@ -0,0 +1,9 @@
+// styled.d.ts
+
+import 'styled-components';
+import { Theme } from './src/settings/theming/theme';
+
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
