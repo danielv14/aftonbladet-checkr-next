7c495eeadcb68b1316c85dde3bf2e5f90cb156d0
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-05-12 20:51:14 +0200

    add middleware to check if todays checkers already scraped

---
+++ src/middlewares/todayAlreadyScrapedMiddleware.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getLatestEntry } from '../lib/firebase/actions';
+import { isToday } from 'date-fns';
+import { CheckerDTO } from '../interfaces/Checker';
+
+const isTodaysCheckersLogged = (latestChecker: CheckerDTO) => latestChecker && isToday(new Date(latestChecker.created));
+
+export const withTodayAlreadyScraped = (handler: Function) => async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const latestChecker = await getLatestEntry();
+    if (isTodaysCheckersLogged(latestChecker)) {
+      return res.status(200).json({ message: `Today's checkers has already been added into db` });
+    }
+    return handler(req, res);
+  } catch (error) {
+    console.log(`Error when checking if todays amount of checkers has been added.`, error);
+    res.status(500).json({ message: error });
+  }
+};
