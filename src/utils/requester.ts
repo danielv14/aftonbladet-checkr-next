1a83e288b45b8cd26cca8f8f8ab6552d2290219c
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-05-19 19:51:40 +0200

    fetch wrapper with typings

--- src/utils/requester.ts
+++ src/utils/requester.ts
@@ -1,15 +1,19 @@
 import fetch from 'isomorphic-unfetch';
-import { Checker } from '../interfaces/Checker';
-import { dtoCheckerToChecker } from './dtoCheckers';
+import { Checker, CheckerDto } from '../interfaces/Checker';
+import { checkerDtoToChecker } from './dtoCheckers';
+
+const fetcher = async <T>(url: string): Promise<T> => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+};
 
 export const getAllCheckers = async (origin: string): Promise<Checker[]> => {
-  const res = await fetch(`${origin}/api/get-checkers`);
-  const { checkers } = await res.json();
-  return checkers.map(dtoCheckerToChecker) as Checker[];
+  const { checkers } = await fetcher<{ checkers: CheckerDto[] }>(`${origin}/api/get-checkers`);
+  return checkers.map(checkerDtoToChecker) as Checker[];
 };
 
 export const getCurrentAmountOfCheckers = async (origin: string): Promise<number> => {
-  const currentCheckersRes = await fetch(`${origin}/api/current-checkers`);
-  const { current } = await currentCheckersRes.json();
-  return current as number;
+  const { current } = await fetcher<{ current: number }>(`${origin}/api/current-checkers`);
+  return current;
 };
