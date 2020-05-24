ae992eaf83c560a78c93640172112a79fbad4fc4
Daniel Vernberg <daniel.vernberg@gmail.com>
2020-05-24 20:48:37 +0200

    rename func

--- src/lib/firebase/seedTestdata.ts
+++ src/lib/firebase/seedTestdata.ts
@@ -2,7 +2,7 @@ import { checkersTestData } from '../../utils/testData/checkersTestData';
 import { addCheckerToDb } from './actions';
 import { getFixedSizeOfSeeds } from './utils';
 
-export const seedDBWithTestdata = async (fullSeed = false) => {
+export const seedDbWithTestdata = async (fullSeed = false) => {
   const seedData = fullSeed ? checkersTestData : getFixedSizeOfSeeds(2);
   const addToDbPromises = seedData.map(addCheckerToDb);
   return Promise.all(addToDbPromises);
